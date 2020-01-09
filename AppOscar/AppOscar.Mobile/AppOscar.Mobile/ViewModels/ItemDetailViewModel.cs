403ec04ef9223bfea4d2d71c75a67703d814fa93
CallebCecco <calleb.malinoski@gmail.com>
2020-01-09 11:54:47 -0300

    Projeto do Mobile

---
+++ AppOscar/AppOscar.Mobile/AppOscar.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppOscar.Mobile.Models;
+
+namespace AppOscar.Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
