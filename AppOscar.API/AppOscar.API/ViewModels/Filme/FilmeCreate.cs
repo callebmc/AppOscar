2112a0d7f99ad8ab4b891163b7233895260d8b52
CallebCecco <calleb.malinoski@gmail.com>
2020-01-29 18:30:10 -0300

    ViewModel inicial de Filme

---
+++ AppOscar.API/AppOscar.API/ViewModels/Filme/FilmeCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.ViewModels.Filme
+{
+    public class FilmeCreate
+    {
+        /// <summary>
+        /// Identificador Unico de Filme
+        /// </summary>
+        public Guid IdFilme { get; set; }
+
+        /// <summary>
+        /// Nome do filme
+        /// </summary>
+        public string NomeFilme { get; set; }
+    }
+}
