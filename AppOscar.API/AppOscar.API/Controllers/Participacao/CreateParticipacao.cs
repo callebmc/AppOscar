67e76b05321d6d8ad434da86b6193b3ade2c49bd
calleb.cecco <calleb.cecco@mps.com.br>
2020-01-30 14:13:32 -0300

    Controller de Participação

---
+++ AppOscar.API/AppOscar.API/Controllers/Participacao/CreateParticipacao.cs
@@ -0,0 +1,71 @@
+﻿using AppOscar.Persistence;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.Controllers.Participacao
+{
+    public class CreateParticipacao : IRequest<CreateParticipacaoResult>
+    {
+        [Required]
+        public Guid IdFilme { get; set; }
+
+        [Required]
+        public Guid IdCategoria { get; set; }
+    }
+
+    public class CreateParticipacaoResult
+    {
+        public DateTime DthCriacao { get; set; }
+
+        public int IdParticipacao { get; set; }
+    }
+
+    public class CreateParticipacaoHandler : IRequestHandler<CreateParticipacao, CreateParticipacaoResult>
+    {
+        private readonly ILogger<CreateParticipacaoHandler> logger;
+        private readonly AppOscarContext context;
+
+        public CreateParticipacaoHandler(ILogger<CreateParticipacaoHandler> logger, AppOscarContext context)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<CreateParticipacaoResult> Handle(CreateParticipacao request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+            {
+                throw new ArgumentNullException(nameof(request));
+            }
+
+            return CreateParticipacaoInternalAsync(request.IdCategoria, request.IdFilme, cancellationToken);
+        }
+
+        private async Task<CreateParticipacaoResult> CreateParticipacaoInternalAsync(Guid idCategoria, Guid idFilme, CancellationToken cancellationToken)
+        {
+            // Validar que a Categoria existe
+            var categoria = await context.Categorias.FindAsync(new object[] { idCategoria }, cancellationToken);
+            if (categoria is null)
+                throw new KeyNotFoundException("Categoria não encontrada");
+
+            // Validar que o Filme existe
+            if ((await context.Filmes.FindAsync(new object[] { idFilme }, cancellationToken)) is null)
+                throw new KeyNotFoundException("Filme não encontrado");
+
+            // Inserir a Partipacao através da Categoria
+            var novaParticipacao = new Models.Participacao { IdCategoria = idCategoria, IdFilme = idFilme };
+            categoria.Participantes.Add(novaParticipacao);
+
+            // Salvar Alterações
+            await context.SaveChangesAsync(cancellationToken);
+
+            // Retornar o result
+            return new CreateParticipacaoResult { DthCriacao = DateTime.Now, IdParticipacao = novaParticipacao.Id };
+        }
+    }
+}
---
+++ AppOscar.API/AppOscar.API/Controllers/Participacao/ParticipacaoController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.Controllers.Participacao
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParticipacaoController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ParticipacaoController(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNew([FromBody] CreateParticipacao request)
+        {
+            if (request is null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await mediator.Send(request);
+
+            return Ok(result.IdParticipacao);
+        }
+    }
+}
