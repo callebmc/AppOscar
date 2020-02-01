2939e50367080fd6e63a1374c20d63dc6bd18daa
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-02-01 16:41:14 -0300

    Adicionar model para Votos

---
+++ AppOscar.API/AppOscar.Models/Voto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AppOscar.Models
+{
+    /// <summary>
+    /// Voto de um usuário sobre a participação de um filme em uma categoria.
+    /// </summary>
+    public class Voto
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset DthCriacao { get; set; }
+
+        public int IdParticipacao { get; set; }
+
+        public virtual Participacao Participacao { get; set; }
+    }
+}
