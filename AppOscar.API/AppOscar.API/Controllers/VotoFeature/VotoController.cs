170dfa9b5d68c4cbe97b33124178cbc1995e8bc4
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2020-02-01 17:17:31 -0300

    Disponibilizar endpoint para novos votos

---
+++ AppOscar.API/AppOscar.API/Controllers/VotoFeature/VotoController.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.Controllers.VotoFeature
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VotoController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public VotoController(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateVoto([FromBody] CreateVoto votoCommand)
+        {
+            try
+            {
+                var result = await mediator.Send(votoCommand);
+                return Ok(result.DthCriacao);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+    }
+}
