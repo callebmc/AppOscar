cc003c42a5a5050b9a570ac18c4908c9e3babe80
CallebCecco <calleb.malinoski@gmail.com>
2020-02-01 10:46:29 -0300

    Altera Controllers de Categorias pra novo padrão

---
+++ AppOscar.API/AppOscar.API/Controllers/CategoriaFeature/CategoriaController.cs
@@ -0,0 +1,54 @@
+﻿using AppOscar.API.Controllers.CategoriaFeature;
+using AppOscar.API.Domain;
+using AppOscar.API.Repositories;
+using AppOscar.API.ViewModels.Categoria;
+using AppOscar.API.ViewModels.Filme;
+using AppOscar.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.Controllers.Parametros
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriaController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public CategoriaController(IMediator mediator, ICategoriaRepository categoriaRepository)
+        {
+            _mediator = mediator;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Categoria>))]
+        public async Task<IActionResult> GetAllCategorias()
+        {
+            var categorias = await _mediator.Send(new ListAllCategorias());
+
+            return Ok(categorias);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategoria([FromBody] CreateCategoria request)
+        {
+            if (request is null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var result = await _mediator.Send(request);
+
+
+            return Ok(result.IdCategoria);
+        }
+    }
+}
