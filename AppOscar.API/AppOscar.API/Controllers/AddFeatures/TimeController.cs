1e620d7eda2b75e485f026630e1927c00aca0766
CallebCecco <calleb.malinoski@gmail.com>
2020-02-06 01:19:02 -0300

    Adiciona controlador de tempo

---
+++ AppOscar.API/AppOscar.API/Controllers/AddFeatures/TimeController.cs
@@ -0,0 +1,37 @@
+﻿using AppOscar.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AppOscar.API.Controllers.AddFeatures
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly AppOscarContext context;
+
+
+        public TimeController(AppOscarContext context)  {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<int>> GetQuantoFalta()
+        {
+            try
+            {
+                DateTime _timeDataOscar = new DateTime(2020, 2, 9, 20, 30, 00);
+                DateTime _timeNow = DateTime.Now;
+                TimeSpan time = _timeDataOscar.Subtract(_timeNow);
+                return new OkObjectResult(time.TotalSeconds);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+    }
+}
